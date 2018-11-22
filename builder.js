bb5c0ce7f28b502561cddc8cb257418d7b1e148e
zhangzhiqiang <745124335@aa.com>
2018-11-22 13:53:12 +0800

    代码优化

--- builder.js
+++ builder.js
@@ -150,7 +150,7 @@ class Msg extends Validator{
         throw new Error("not implement");
     }
     constructor(type) {
-       super();
+        super();
        this.type = type
     }
     GetMsg(){
