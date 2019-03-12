615562789a338a68f5873a360c0e9b3fd486a7f0
zhangzhiqiang <745124335@aa.com>
2019-03-12 16:06:56 +0800

    support change network

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const Crypto = require("./crypto");
 const Builder = require("./builder").Builder;
 const config = require('./config');
 
-function getBuilder(chainName){
+function getBuilder(chainName,network){
+    setNetwork(network);
     return Builder.getBuilder(chainName)
 }
 
-function getCrypto(chainName){
+function getCrypto(chainName,network){
+    setNetwork(network);
     return Crypto.getCrypto(chainName)
+}
+
+function setNetwork(network){
+    if(network && network === 'testnet'){
+        config.iris.bech32 = {
+            accAddr: "faa",
+            valAddr: "fva",
+            accPub: "fap"
+        };
+    }
+    return config
 }
 
 module.exports = {getCrypto,getBuilder,config};
